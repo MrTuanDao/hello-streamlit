eb470789cc683df33687ec7da8e27695a4cd08aa
Đào Quốc Tuấn <103438257+MrTuanDao@users.noreply.github.com>
2024-04-08 03:08:40 +0000

    a

--- Hello.py
+++ Hello.py
@@ -4,7 +4,22 @@ from io import StringIO
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
+    st.write('Labels') 
+
     # To read file as bytes:
     st.image(uploaded_file)
 
-    st.write('Labels')
+    st.write('Data')
+
+import torch
+from torchvision import models
+
+# Kiểm tra xem có GPU có sẵn không và đặt PyTorch sử dụng GPU nếu có
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Load mô hình ResNet pre-trained
+resnet = models.resnet18()
+
+# Chuyển mô hình đến GPU nếu có sẵn
+resnet = resnet.to(device)
+
